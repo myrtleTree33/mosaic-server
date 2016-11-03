33105fe616c21591ce7ca6da16ce85973b462420
myrtletree33 <me@joeltong.org>
2016-11-03 17:02:09 +0800

    changed URL and exposed num_clusters option

--- api/controllers/MosaicController.js
+++ api/controllers/MosaicController.js
@@ -12,19 +12,21 @@ module.exports = {
   generateMosaic: (req, res) => {
     let type = req.body.type;
     let base64Img = req.body.base64Img;
-    if (!type || !base64Img) {
+    let numClusters = req.body.numClusters;
+    if (!type || !base64Img)er  {
       return res.badRequest('Missing type or base 64 image.');
     }
     client.invoke("generate_mosaic",
       base64Img,
-      20,
+      numClusters,
       48,
       12,
 			type,
       function(err, outputBase64ImgStr, more) {
         res.status(200).json({
 					type: type,
-					base64Img:outputBase64ImgStr
+					base64Img:outputBase64ImgStr,
+          numClusters: numClusters
         });
       });
   }
